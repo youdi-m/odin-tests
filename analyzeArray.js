2067ec99e717edd1f9fb4ff23c6eb3ed3d547c18
youdi <mehdi.youssef97@gmail.com>
2026-04-22 08:14:12 -0400

    finished tests

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,7 +1,9 @@
 export function analyze(arr) {
-	if (!arr) return arr;
+	if (arr === null) return null;
+	if (arr === undefined) return undefined;
+	if (arr.length == 0) return null;
 
-	var average = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+	var average = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0)/arr.length
 	var min = Math.min(...arr)
 	var max = Math.max(...arr)
 	var length = arr.length
